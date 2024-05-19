5be473f6ff5d25c622f76187d0b6e54affbfa011
Filip Filchev <fkfilchev@gmail.com>
2024-05-19 17:23:45 +0300

    Fix Lint

--- src/app/games/shared/components/menu/menu.component.spec.ts
+++ src/app/games/shared/components/menu/menu.component.spec.ts
@@ -8,7 +8,7 @@ describe('MenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenuComponent]
+      imports: [MenuComponent],
     })
     .compileComponents();
     
